d2e0ff775d0005dcadaf1e0c40694284b93761ec
hafdhisofien <1114@holbertonschool.com>
2020-07-04 22:44:46 +0000

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,8 +15,10 @@ def teardown_request(self):
     """ Method to close session """
     storage.close()
 
-
-
+@app.errorhandler(404)
+def page_not_found(error):
+    """ handler for 404 errors """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 host = getenv('HBNB_API_HOST') or '0.0.0.0'
 port = getenv('HBNB_API_PORT') or '5000'
