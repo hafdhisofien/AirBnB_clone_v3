110ad2325378ca24c1f2079bb3cc7a111dc73c73
hafdhisofien <1114@holbertonschool.com>
2020-07-04 22:35:15 +0000

    counter

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,14 @@ from models import storage
 def status():
     """ returns a JSON: "status": "OK" """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """endpoint that retrieves the number of each objects by type """
+    counter = {"amenities": storage.count("Amenity"),
+                "cities": storage.count("City"),
+                "places": storage.count("Place"),
+                "reviews": storage.count("Review"),
+                "states": storage.count("State"),
+                "users": storage.count("User")}
+    return jsonify(counter)
