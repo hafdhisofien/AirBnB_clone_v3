6745507d222888d53eb3fc6c51bad52ad2ce9002
hafdhisofien <1114@holbertonschool.com>
2020-07-05 13:15:56 +0000

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,12 @@ from models import storage
 from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -15,10 +18,12 @@ def teardown_request(self):
     """ Method to close session """
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     """ handler for 404 errors """
     return make_response(jsonify({"error": "Not found"}), 404)
+
 
 host = getenv('HBNB_API_HOST') or '0.0.0.0'
 port = getenv('HBNB_API_PORT') or '5000'
