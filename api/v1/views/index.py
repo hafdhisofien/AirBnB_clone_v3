a63167f905e2e37903785892884b71eaf0437f2d
hafdhisofien <1114@holbertonschool.com>
2020-07-05 13:23:59 +0000

    pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,19 +6,19 @@ from flask import jsonify
 from models import storage
 
 
-
 @app_views.route('/status')
 def status():
     """ returns a JSON: "status": "OK" """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats')
 def stats():
     """endpoint that retrieves the number of each objects by type """
     counter = {"amenities": storage.count("Amenity"),
-                "cities": storage.count("City"),
-                "places": storage.count("Place"),
-                "reviews": storage.count("Review"),
-                "states": storage.count("State"),
-                "users": storage.count("User")}
+               "cities": storage.count("City"),
+               "places": storage.count("Place"),
+               "reviews": storage.count("Review"),
+               "states": storage.count("State"),
+               "users": storage.count("User")}
     return jsonify(counter)
